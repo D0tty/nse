db7c77301c883870810c296eaa4619da301f3016
Victor Chanfrault <victor.chanfrault@outlook.fr>
2020-12-15 14:52:53 +0100

    iso supprimé, on reste sur tar

--- container.sh
+++ container.sh
@@ -21,8 +21,10 @@ for pkg in cgcreate cgset cgexec; do
     find_package "$pkg"
 done
 
-# DL conteneur
-if [ ! -f "$archive" ]; then
+# parse args
+if [ -f "$1" ]; then
+    archive="$(realpath $1)"
+elif [ ! -f "$archive" ]; then # DL conteneur
     echo "Downloading fish container..."
     wget -nv "$url" -O "$archive"
 else
@@ -50,11 +52,14 @@ sudo cgset -r memory.limit_in_bytes=1000000000 "$cgroup_id"
 # sudo cgget "$cgroup_id" # affiche les details du cgroupe
 
 # run container
+cmd="
+    export \"PATH=/bin:$PATH\" &&
+    /bin/mount -t proc proc /proc &&
+    /bin/hostname mycontainer &&
+    /usr/bin/fish"
+
 sudo cgexec -g "$controllers:$cgroup_id" \
     unshare -fmuipn --mount-proc \
     chroot "$PWD" \
-    /bin/sh -c "
-        export \"PATH=/bin:$PATH\" &&
-        /bin/mount -t proc proc /proc &&
-        /bin/hostname mycontainer &&
-        /usr/bin/fish"
+    /bin/sh -c "$cmd"
+
